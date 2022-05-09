e655681b63f4f52183ac0805c1b3d2fb11b19a9e
Miriam <miriamlunacarreon133@gmail.com>
2022-05-09 14:13:23 -0500

    Primer método: filterByMission

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Reader = require("./lib/utils/Reader")
+const ExplorerService = require("./lib/services/ExplorerService")
 
 const explorers = Reader.readJsonFile("explorers.json")
-console.log(explorers)
+//console.log(explorers)
+
+//console.log(ExplorerService.filterByMission(explorers, "node"));
+ExplorerService.filterByMission(explorers, "node");
+//ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
