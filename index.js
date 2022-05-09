1f7848aa86590b62b90b5dd8942853760145b5c1
Miriam <miriamlunacarreon133@gmail.com>
2022-05-09 15:10:38 -0500

    Funciones getAmountOfExplorersByMission y getExplorersUsernamesByMission

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
 
 const explorers = Reader.readJsonFile("explorers.json");
-//console.log(explorers)
+//console.log(explorers.length)
 
 //console.log(ExplorerService.filterByMission(explorers, "node"));
 ExplorerService.filterByMission(explorers, "node");
-//ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+//console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
--- tests/services/ExplorerService.test.js
+++ tests/services/ExplorerService.test.js
@@ -6,4 +6,17 @@ describe("Tests para ExplorerService", ()=>{
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });  
+
+    test("Requerimiento 2: Obtener la cantidad de explorers que están en node.",()=>{
+        const explorers = [{mission: "node"}];
+        const explorersInNodeLength = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(explorersInNodeLength).toBe(1);
+    });
+
+    test("Requerimiento 3: Obtener la lista de usuarios de github de los explorers que están en node.", ()=>{
+        const explorers = [{mission: "node", githubUsername: "username"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+        expect(explorersInNode).toContain("username");
+    });
 });
